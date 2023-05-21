c4eac110ad2ef051295b9a350aa84418424c427c
bymia <wanderfly336@gmail.com>
2023-05-21 15:24:27 +0800

    button tag

--- script.js
+++ script.js
@@ -1,23 +1,58 @@
-var _ = require('lodash');
+var currentCss = document.querySelector("h3");
+var inputColor1 = document.querySelector(".color1");
+var inputColor2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");//body in body tag
+var rightArrow = document.querySelector(".right");
+var leftArrow = document.querySelector(".left");
 
-var array = [1,2,3,4,5,6,7,8];
-console.log('answer:', _.without(array, 3));
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
+function getRandomColor() {
+   var letters = "0123456789ABCDEF";
+   var color = "#";
+
+   for (var i = 0; i < 6; i++) {
+       color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
 function setGradient() {
 	body.style.background = 
 	"linear-gradient(to right, " 
-	+ color1.value 
+	+ inputColor1.value 
 	+ ", " 
-	+ color2.value 
-	+ ")";
+	+ inputColor2.value 
+	+ ")";//syntax
 
-	css.textContent = body.style.background + ";";
+	currentCss.textContent = body.style.background + ";";
 }
 
-color1.addEventListener("input", setGradient);
+function setRandomGradient() {
+    var randomColor2 = getRandomColor();
 
-color2.addEventListener("input", setGradient);
+    body.style.background = 
+    "linear-gradient(to right, " 
+    + inputColor1.value 
+    + ", " 
+    + randomColor2 
+    + ")";
+    inputColor2.value = randomColor2;
+    currentCss.textContent = body.style.backgroundImage + ";";
+}
+
+function setPreviousGradient() {
+    var randomColor1 = getRandomColor();
+ 
+    body.style.background = 
+    "linear-gradient(to right, " 
+    + randomColor1 
+    + ", " 
+    + inputColor2.value 
+    + ")";
+    inputColor1.value = randomColor1;
+    currentCss.textContent = body.style.backgroundImage + ";";
+}
+
+inputColor1.addEventListener("input", setGradient);
+inputColor2.addEventListener("input", setGradient);
+rightArrow.addEventListener("click", setRandomGradient);
+leftArrow.addEventListener("click", setPreviousGradient);
